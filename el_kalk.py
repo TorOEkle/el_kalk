0cfd04eaf8d164d370011ae38049535090cb2750
Tor Odin Ekle <117438207+TorOEkle@users.noreply.github.com>
2022-12-13 14:40:17 +0100

    Update el_kalk.py

--- el_kalk.py
+++ el_kalk.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-st.set_page_config( page_title= 'Maries strøm kalkulator', page_icon= 'mag')
+st.set_page_config( page_title= 'Maries strøm kalkulator', page_icon= 'electric_plug')
 st.header('Maries strøm kalkulator')
 
 dict = {'Vaskemaskin': 2.250,
